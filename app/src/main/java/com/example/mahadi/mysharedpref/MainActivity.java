d77d6bfa225395430a200cc21a65662942495742
codermahadi <31430772+codermahadi@users.noreply.github.com>
2018-03-12 12:17:20 +0600

    Store data useng sgaredPreference class

--- app/src/main/java/com/example/mahadi/mysharedpref/MainActivity.java
+++ app/src/main/java/com/example/mahadi/mysharedpref/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.mahadi.mysharedpref;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     private EditText uname,password;
     private Button sava,load;
@@ -25,5 +29,38 @@ public class MainActivity extends AppCompatActivity {
         result = (TextView) findViewById(R.id.result);
 
 
+        sava.setOnClickListener(this);
+        load.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        if (view.getId() == R.id.save) {
+
+            String username = uname.getText().toString();
+            String Upass = password.getText().toString();
+
+
+            if (username.equals("") && Upass.equals("")){
+
+                Toast.makeText(this,"Please Input Username and Password", Toast.LENGTH_LONG).show();
+            }else {
+                SharedPreferences sharedPreferences = getSharedPreferences("userDetails", Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putString("UsernameKey", username);
+                editor.putString("PasswordKey", Upass);
+                editor.commit();
+                Toast.makeText(this,"Data Stored successfully", Toast.LENGTH_LONG).show();
+
+            }
+
+        }else if (view.getId() == R.id.load){
+
+        }else{
+
+        }
     }
 }
