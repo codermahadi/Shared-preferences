1a893bd9f26c4ebbc5fb8e7e8529346d0b9e6609
codermahadi <31430772+codermahadi@users.noreply.github.com>
2018-03-12 11:31:30 +0600

    create object

--- app/src/main/java/com/example/mahadi/mysharedpref/MainActivity.java
+++ app/src/main/java/com/example/mahadi/mysharedpref/MainActivity.java
@@ -8,11 +8,22 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    private EditText uname,password;
+    private Button sava,load;
+    private TextView result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        uname = (EditText) findViewById(R.id.uname);
+        password = (EditText) findViewById(R.id.password);
+        sava = (Button) findViewById(R.id.save);
+        load = (Button) findViewById(R.id.load);
+        result = (TextView) findViewById(R.id.result);
+
+
     }
 }
